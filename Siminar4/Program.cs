05d0125605d8e63a944c7cd16c42e82404f4e815
Tatsiana <yasi-anka@bk.ru>
2022-11-07 10:18:50 +0300

    Семинар 4

---
+++ Siminar4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (А)
+// и выдаёт сумму чисел от 1 до А.
+
+int FindSum(int number)
+{
+    int sum = 0;
+
+    for(int current = 1; current <= number; current++)
+    sum += current;
+
+    return sum;
+}
+
+Console.Write("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Сумма числел равна " + FindSum(a));
