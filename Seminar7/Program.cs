dcc85680629be84be43705d8c315dfc29ea417ea
Tatsiana <yasi-anka@bk.ru>
2022-11-22 16:04:03 +0300

    Исправление ошибки в в методе ПЕЧАТЬ МАССИВА

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -16,7 +16,7 @@ void Show2dArray(int[,] array)// печать двумерного массив�
 {
     for(int i = 0; i < array.GetLength(0); i++)
     {
-        for(int j = 0; j < array.GetLength(0); j++)
+        for(int j = 0; j < array.GetLength(1); j++)
         Console.Write(array[i,j] + " ");
 
     Console.WriteLine();
